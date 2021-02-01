125fd2adca70f05068efd974bfbb5f0a7f810862
jas010i <63612124+jas010i@users.noreply.github.com>
2021-02-01 02:54:45 -0600

    l

---
+++ JS/menuDrop.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+function expandMenu() {
+
+
+            document.getElementById('expand').style.display = "block";
+
+            document.getElementById('expand-toggle').style.display = "none";
+
+            document.getElementById('expand-detoggle').style.display = "inline-block";
+
+
+}
+
+
+
+
+
+
+function retractMenu() {
+
+    document.getElementById('expand').style.display = "none";
+
+    document.getElementById('expand-detoggle').style.display = "none";
+
+    document.getElementById('expand-toggle').style.display = "inline-block";
+
+
+
+
+}
