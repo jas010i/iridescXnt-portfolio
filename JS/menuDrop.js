60914a2254a30a0d0e753f32bb9764ce509add6f
jas010i <jas010i@users.noreply.github.com>
2021-02-02 18:22:33 -0600

    r

--- JS/menuDrop.js
+++ JS/menuDrop.js
@@ -17,9 +17,6 @@ function expandMenu() {
 
 }
 
-
-
-
 // Closes header menu
 
 function retractMenu() {
@@ -30,7 +27,31 @@ function retractMenu() {
 
     document.getElementById('expand-toggle').style.display = "inline-block";
 
+}
 
+
+// on click shows information (static v dynamic)
+
+function arrDown() {
+
+    document.getElementById('static-dynamic-info').style.display = "block";
+
+    document.getElementById('arr-down').style.display = "none";
+
+    document.getElementById('arr-up').style.display = "block";
+
+}
+
+
+// on click hides information (static v dynamic)
+
+function arrUp() {
+
+    document.getElementById('static-dynamic-info').style.display = "none";
+
+    document.getElementById('arr-down').style.display = "block";
+
+    document.getElementById('arr-up').style.display = "none";
 
 
 }
